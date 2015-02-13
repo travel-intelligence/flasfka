9e0be0dd97d4e52dbcdd7aebfc945ff546b410f2
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-02-13 16:44:33 +0100

    basics are working

--- flasfka/api.py
+++ flasfka/api.py
@@ -4,13 +4,18 @@ from __future__ import with_statement
 import flask
 import xdg.BaseDirectory
 from flasfka import app
+from kafka import SimpleProducer, KafkaClient, SimpleConsumer, KeyedProducer
 
 APP_NAME = "flasfka"
 XDG_CONFIG_DIR = xdg.BaseDirectory.save_config_path(APP_NAME)
 XDG_DATA_DIR = xdg.BaseDirectory.save_data_path(APP_NAME)
 
+
 app.config.update(dict(
-    HOSTS=["localhost:9092"]
+    HOSTS=["localhost:9092"],
+    DEFAULT_GROUP="flasfka",
+    CONSUMER_TIMEOUT=1,
+    CONSUMER_LIMIT=20
     ))
 
 
@@ -38,30 +43,57 @@ def json_response(status, body={}):
     response.status = str(status)
     return response
 
-def get_kafka():
-    if not hasattr(flask.g, "kafka"):
-        flask.g.kafka = KafkaClient(app.config["HOSTS"])
-    return flask.g.kafka
 
-def kafka_post(topic, message, key=None):
-    kafka = get_kafka()
-    if key is None:
-        producer = SimpleProducer(kafka)
-        producer.send_messages(topic, message)
-    else:
-        producer = KeyedProducer(kafka)
-        producer.send(topic, key, message)
-
-def kafka_get(topic, group=None):
-    kafka = get_kafka()
+def get_kafka_client():
+    if not hasattr(flask.g, "kafka_client"):
+        flask.g.kafka_client = KafkaClient(app.config["HOSTS"])
+    return flask.g.kafka_client
 
 
-@app.route("/", methods=["GET", "POST"])
+def get_kafka_producer():
+    if not hasattr(flask.g, "kafka_producer"):
+        client = get_kafka_client()
+        flask.g.kafka_producer = KeyedProducer(client, batch_send=True)
+    return flask.g.kafka_producer
+
+
+def produce(topic, message, key=None):
+    message = message.encode("utf-8")
+    producer = get_kafka_producer()
+    producer.send(topic, key, message)
+
+
+def consume(topic, group=app.config["DEFAULT_GROUP"].encode("utf-8"), limit=20):
+    client = get_kafka_client()
+    consumer = SimpleConsumer(client, group, topic,
+            iter_timeout=app.config["CONSUMER_TIMEOUT"])
+    res = []
+    for n, message in enumerate(consumer):
+        res.append(message.message.value.decode("utf-8"))
+        if n >= limit - 1:
+            break
+    return res
+
+
 @app.route("/<topic>/", methods=["GET", "POST"])
-@app.route("/<group>/<topic>/", methods=["GET", "POST"])
-def flasfka(group=None, topic=None):
-    kafka_send("hello", "world")
-    return json_response(200, "hello")
+@app.route("/<topic>/<group_or_key>/", methods=["GET", "POST"])
+def flasfka(topic, group_or_key=None):
+
+    topic = topic.encode("utf-8")
+    if group_or_key is not None:
+        group_or_key = group_or_key.encode("utf-8")
+
+    client = get_kafka_client()
+    client.ensure_topic_exists(topic)
+
+    if flask.request.method == "GET":
+        group = group_or_key
+        return json_response(200, consume(topic, group))
+    if flask.request.method == "POST":
+        key = group_or_key
+        produce(topic, flask.request.get_data(), key)
+        return flask.make_response(("", 204, {}))
+        #return json_response(200, "hello")
 
 
 @app.after_request
