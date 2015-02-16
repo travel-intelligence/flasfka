8642eca113aee65d94ed3aa36a527a59fb52da46
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-02-16 14:10:13 +0100

    Minimizing code

--- flasfka/api.py
+++ flasfka/api.py
@@ -19,31 +19,6 @@ app.config.update(dict(
     ))
 
 
-def json_response(status, body={}):
-    """
-    Builds a response object with the content type set to json
-    """
-    status_message = {
-        200: "OK",
-        201: "Created",
-        204: "No Content",
-        400: "Bad Request",
-        403: "Forbidden",
-        404: "Not found",
-        413: "Request entity too large",
-        429: "Too many requests",
-        500: "Internal error"
-        }
-    if not isinstance(body, dict):
-        body = {"message": body}
-    if status != 200:
-        body["status"] = status_message[status]
-        body["status_code"] = status
-    response = flask.jsonify(body)
-    response.status = str(status)
-    return response
-
-
 def get_kafka_client():
     if not hasattr(flask.g, "kafka_client"):
         flask.g.kafka_client = kafka.KafkaClient(app.config["HOSTS"])
@@ -69,9 +44,9 @@ def consume(topic, group, limit=20):
     client = get_kafka_client()
     consumer = kafka.SimpleConsumer(client, group, topic,
             iter_timeout=app.config["CONSUMER_TIMEOUT"])
-    res = []
+    res = {"group": group.decode("utf-8"), "messages": []}
     for n, message in enumerate(consumer):
-        res.append(message.message.value.decode("utf-8"))
+        res["messages"].append(message.message.value.decode("utf-8"))
         if n >= limit - 1:
             break
     return res
@@ -90,12 +65,11 @@ def flasfka(topic, group_or_key=None):
 
     if flask.request.method == "GET":
         group = group_or_key
-        return json_response(200, consume(topic, group))
+        return flask.jsonify(consume(topic, group))
     if flask.request.method == "POST":
         key = group_or_key
         produce(topic, flask.request.get_data(), key)
         return flask.make_response(("", 204, {}))
-        #return json_response(200, "hello")
 
 
 @app.after_request
