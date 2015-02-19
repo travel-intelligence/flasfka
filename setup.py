cd8e3ef7358d7dd6515ba912257cf26296a387d6
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-02-19 23:03:33 +0100

    Adding publishing

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-
+"""
+Push/Pull on Kafka over HTTP
+"""
 
 from setuptools import setup
+from subprocess import check_output as run
+import sys
+import os
+
+VERSION = "1.0.0"
+
+if os.getenv("TRAVIS") is not None:
+    GIT_TAG = run(["git", "describe"]).decode().strip()
+    if VERSION != GIT_TAG:
+        sys.exit("The git tag does not match the release. Please fix.")
 
 setup(
     name="flasfka",
-    version='0.0.1',
-    long_description=__doc__,
+    version=VERSION,
+    long_description=open("README.rst").read(),
+    author="Christophe-Marie Duquesne",
+    author_email="chmd@chmd.fr"
+    url="https://github.com/travel-intelligence/flasfka",
+    download_url="https://github.com/travel-intelligence/flasfka/archive/%s.tar.gz" % VERSION,
     packages=["flasfka"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        ],
     install_requires=[
         "Flask >= 0.10",
         "kafka-python >= 0.9.2",
